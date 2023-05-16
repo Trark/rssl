14efde3e195561e13308a76d271521dbb225b3ad
Trark <trarkium@gmail.com>
2023-05-16 21:32:14 +0100

    Add shaders in capsaicin sources that build successfully as a test

---
+++ hlsl/tests/capsaicin/mod.rs
@@ -0,0 +1,148 @@
+use super::compile_file;
+
+pub const FILES: &[(&str, &str)] = &[
+    source!("capsaicin/convolve_ibl.comp"),
+    source!("capsaicin/convolve_ibl.comp"),
+    source!("capsaicin/convolve_ibl.frag"),
+    source!("capsaicin/convolve_ibl.vert"),
+    source!("capsaicin/dump_copy_aov_to_buffer.comp"),
+    source!("components/blue_noise_sampler/blue_noise_sampler.hlsl"),
+    source!("components/light_sampler/gather_area_lights.geom"),
+    source!("components/light_sampler/gather_area_lights.vert"),
+    source!("components/light_sampler/light_sampler_uniform.hlsl"),
+    source!("components/light_sampler/light_sampler.hlsl"),
+    source!("components/light_sampler_bounds/light_sampler_bounds_shared.h"),
+    source!("components/light_sampler_bounds/light_sampler_bounds.comp"),
+    source!("components/light_sampler_bounds/light_sampler_bounds.hlsl"),
+    source!("components/stratified_sampler/stratified_sampler.hlsl"),
+    source!("lights/light_evaluation.hlsl"),
+    source!("lights/light_sampling_volume.hlsl"),
+    source!("lights/light_sampling.hlsl"),
+    source!("lights/lights_shared.h"),
+    source!("lights/lights.hlsl"),
+    source!("lights/reservoir.hlsl"),
+    source!("materials/material_evaluation.hlsl"),
+    source!("materials/material_sampling.hlsl"),
+    source!("materials/materials.hlsl"),
+    source!("math/sampling.hlsl"),
+    source!("math/color.hlsl"),
+    source!("math/geometry.hlsl"),
+    source!("math/hash.hlsl"),
+    source!("math/math_constants.hlsl"),
+    source!("math/math.hlsl"),
+    source!("math/pack.hlsl"),
+    source!("math/quaternion.hlsl"),
+    source!("math/random.hlsl"),
+    source!("math/sampling.hlsl"),
+    source!("math/spherical_harmonics.hlsl"),
+    source!("render_techniques/atmosphere/atmosphere.comp"),
+    source!("render_techniques/atmosphere/atmosphere.hlsl"),
+    source!("render_techniques/gi10/gi_denoiser.hlsl"),
+    source!("render_techniques/gi10/gi10_shared.h"),
+    source!("render_techniques/gi10/gi10.comp"),
+    source!("render_techniques/gi10/gi10.frag"),
+    source!("render_techniques/gi10/gi10.hlsl"),
+    source!("render_techniques/gi10/gi10.vert"),
+    source!("render_techniques/gi10/hash_grid_cache.hlsl"),
+    source!("render_techniques/gi10/screen_probes.hlsl"),
+    source!("render_techniques/gi10/world_space_restir.hlsl"),
+    source!("render_techniques/path_tracer/reference_pt_shared.h"),
+    source!("render_techniques/path_tracer/reference_pt.comp"),
+    source!("render_techniques/skybox/skybox.frag"),
+    source!("render_techniques/skybox/skybox.vert"),
+    source!("render_techniques/ssgi/ssgi_debug.comp"),
+    source!("render_techniques/ssgi/ssgi_shared.h"),
+    source!("render_techniques/ssgi/ssgi.comp"),
+    source!("render_techniques/taa/taa.comp"),
+    source!("render_techniques/taa/update_history.comp"),
+    source!("render_techniques/tone_mapping/tone_mapping.comp"),
+    source!("render_techniques/visibility_buffer/disocclusion_mask.comp"),
+    source!("render_techniques/visibility_buffer/visibility_buffer.frag"),
+    source!("render_techniques/visibility_buffer/visibility_buffer.vert"),
+    source!("utilities/gpu_reduce.comp"),
+    source!("utilities/gpu_sort.comp"),
+    source!("utilities/FFX_ParallelSort.h"),
+    source!("gpu_shared.h"),
+    source!("mesh.hlsl"),
+];
+
+#[test]
+fn compile_convolve_ibl_comp() {
+    compile_file("capsaicin/convolve_ibl.comp", FILES);
+}
+
+#[test]
+fn compile_convolve_ibl_frag() {
+    compile_file("capsaicin/convolve_ibl.frag", FILES);
+}
+
+#[test]
+fn compile_convolve_ibl_vert() {
+    compile_file("capsaicin/convolve_ibl.vert", FILES);
+}
+
+#[test]
+fn compile_dump_copy_aov_to_buffer() {
+    compile_file("capsaicin/dump_copy_aov_to_buffer.comp", FILES);
+}
+
+#[test]
+fn compile_gather_area_lights_vert() {
+    compile_file("components/light_sampler/gather_area_lights.vert", FILES);
+}
+
+#[test]
+fn compile_skybox_frag() {
+    compile_file("render_techniques/skybox/skybox.frag", FILES);
+}
+
+#[test]
+fn compile_skybox_vert() {
+    compile_file("render_techniques/skybox/skybox.vert", FILES);
+}
+
+#[test]
+fn compile_ssgi_debug() {
+    compile_file("render_techniques/ssgi/ssgi_debug.comp", FILES);
+}
+
+#[test]
+fn compile_ssgi() {
+    compile_file("render_techniques/ssgi/ssgi.comp", FILES);
+}
+
+#[test]
+fn compile_taa() {
+    compile_file("render_techniques/taa/taa.comp", FILES);
+}
+
+#[test]
+fn compile_update_history() {
+    compile_file("render_techniques/taa/update_history.comp", FILES);
+}
+
+#[test]
+fn compile_tone_mapping() {
+    compile_file("render_techniques/tone_mapping/tone_mapping.comp", FILES);
+}
+
+#[test]
+fn compile_disocclusion_mask() {
+    compile_file(
+        "render_techniques/visibility_buffer/disocclusion_mask.comp",
+        FILES,
+    );
+}
+
+#[test]
+fn compile_visibility_buffer_vert() {
+    compile_file(
+        "render_techniques/visibility_buffer/visibility_buffer.vert",
+        FILES,
+    );
+}
+
+#[test]
+fn compile_gpu_reduce() {
+    compile_file("utilities/gpu_reduce.comp", FILES);
+}
---
+++ hlsl/tests/external.rs
@@ -0,0 +1,129 @@
+use rssl_text::*;
+
+struct Minipath<'s>(Vec<&'s str>);
+
+impl<'s> Minipath<'s> {
+    fn new(name: &'s str) -> Self {
+        Minipath(name.split('/').collect::<Vec<_>>())
+    }
+
+    fn push(&mut self, part: &'s str) {
+        if part == ".." {
+            self.0
+                .pop()
+                .expect("test code doesn't expect to make bad paths");
+        } else {
+            self.0.push(part)
+        }
+    }
+
+    fn remove_end(mut self) -> Self {
+        self.0.pop();
+        self
+    }
+
+    fn join(mut self, other: Minipath<'s>) -> Self {
+        for part in other.0 {
+            self.push(part);
+        }
+        self
+    }
+
+    fn generate(&self) -> String {
+        let mut s = String::new();
+        if let Some((end, rest)) = self.0.split_last() {
+            for part in rest {
+                s.push_str(part);
+                s.push('/');
+            }
+            s.push_str(end);
+        }
+        s
+    }
+}
+
+#[track_caller]
+fn compile_file(entry_file: &str, files: &'static [(&'static str, &'static str)]) {
+    // Create source manager to store the source into
+    let mut source_manager = SourceManager::new();
+
+    /// A file loader that loads from static strings
+    struct TestIncludeHandler {
+        pub files: &'static [(&'static str, &'static str)],
+    }
+
+    impl IncludeHandler for TestIncludeHandler {
+        fn load(&mut self, file_name: &str, parent_name: &str) -> Result<FileData, IncludeError> {
+            let file_path = Minipath::new(file_name);
+            let parent_path = Minipath::new(parent_name).remove_end();
+
+            let absolute_name = file_path.generate();
+            let relative_path = parent_path.join(file_path);
+            let relative_name = relative_path.generate();
+            assert_eq!(absolute_name, file_name);
+
+            for &(entry_name, entry_contents) in self.files {
+                if entry_name == relative_name {
+                    return Ok(FileData {
+                        real_name: entry_name.to_string(),
+                        contents: entry_contents.to_string(),
+                    });
+                }
+            }
+
+            for &(entry_name, entry_contents) in self.files {
+                if entry_name == absolute_name {
+                    return Ok(FileData {
+                        real_name: entry_name.to_string(),
+                        contents: entry_contents.to_string(),
+                    });
+                }
+            }
+
+            Err(IncludeError::FileNotFound)
+        }
+    }
+
+    let mut include_handler = TestIncludeHandler { files };
+
+    // Preprocess the text
+    let tokens =
+        match rssl_preprocess::preprocess(entry_file, &mut source_manager, &mut include_handler) {
+            Ok(tokens) => tokens,
+            Err(err) => panic!("{}", err.display(&source_manager)),
+        };
+
+    let tokens = rssl_preprocess::prepare_tokens(&tokens);
+
+    // Run the parser
+    let tree = match rssl_parser::parse(&tokens) {
+        Ok(tree) => tree,
+        Err(err) => panic!("{}", err.display(&source_manager)),
+    };
+
+    // Run the type checker
+    let ir = match rssl_typer::type_check(&tree) {
+        Ok(ir) => ir,
+        Err(err) => panic!("{}", err.display(&source_manager)),
+    };
+
+    let ir = ir.assign_api_bindings(rssl_ir::AssignBindingsParams::default());
+
+    match rssl_hlsl::export_to_hlsl(&ir) {
+        Ok(_) => {
+            // Success!
+        }
+        Err(err) => {
+            // TODO: Error printing
+            panic!("{err:?}")
+        }
+    }
+}
+
+macro_rules! source {
+    ($file:expr) => {
+        ($file, include_str!($file))
+    };
+}
+
+mod capsaicin;
