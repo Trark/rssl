308376998e3cd2f84f3b226b754905f4769eb6ef
Trark <trarkium@gmail.com>
2023-06-04 17:03:35 +0100

    Add register space to object types test to complicate it

--- hlsl/tests/object_types.hlsl
+++ hlsl/tests/object_types.hlsl
@@ -3,20 +3,20 @@ RWByteAddressBuffer g_byteBufferOutput : register(u1);
 Buffer<float4> g_texelBuffer : register(t2);
 RWBuffer<uint3> g_texelBufferOutput : register(u3);
 Texture2D<float4> g_texture : register(t4);
-RWTexture2D<float2> g_textureOutput : register(u5);
-StructuredBuffer<uint> g_primitiveStructuredBuffer : register(t6);
-RWStructuredBuffer<uint2> g_primitiveStructuredBufferOutput : register(u7);
+RWTexture2D<float2> g_textureOutput : register(u0, space1);
+StructuredBuffer<uint> g_primitiveStructuredBuffer : register(t1, space1);
+RWStructuredBuffer<uint2> g_primitiveStructuredBufferOutput : register(u5);
 
 struct S
 {
     float4x4 mat;
 };
 
-StructuredBuffer<S> g_complexStructuredBuffer : register(t8);
-RWStructuredBuffer<S> g_complexStructuredBufferOutput : register(u9);
-ConstantBuffer<S> g_cb : register(b10);
+StructuredBuffer<S> g_complexStructuredBuffer : register(t6);
+RWStructuredBuffer<S> g_complexStructuredBufferOutput : register(u7);
+ConstantBuffer<S> g_cb : register(b8);
 
-cbuffer g_cb : register(b11)
+cbuffer g_cb : register(b9)
 {
     S s;
 }
--- hlsl/tests/object_types.vk.hlsl
+++ hlsl/tests/object_types.vk.hlsl
@@ -3,27 +3,27 @@ struct InlineDescriptor0
     [[vk::offset(0)]] uint64_t g_byteBuffer;
     [[vk::offset(8)]] uint64_t g_byteBufferOutput;
 };
-[[vk::binding(10)]] ConstantBuffer<InlineDescriptor0> g_inlineDescriptor0;
+[[vk::binding(8)]] ConstantBuffer<InlineDescriptor0> g_inlineDescriptor0;
 
 static const uint64_t g_byteBuffer = g_inlineDescriptor0.g_byteBuffer;
 static const uint64_t g_byteBufferOutput = g_inlineDescriptor0.g_byteBufferOutput;
 [[vk::binding(0)]] Buffer<float4> g_texelBuffer;
 [[vk::binding(1)]] RWBuffer<uint3> g_texelBufferOutput;
 [[vk::binding(2)]] Texture2D<float4> g_texture;
-[[vk::binding(3)]] RWTexture2D<float2> g_textureOutput;
-[[vk::binding(4)]] StructuredBuffer<uint> g_primitiveStructuredBuffer;
-[[vk::binding(5)]] RWStructuredBuffer<uint2> g_primitiveStructuredBufferOutput;
+[[vk::binding(0, 1)]] RWTexture2D<float2> g_textureOutput;
+[[vk::binding(1, 1)]] StructuredBuffer<uint> g_primitiveStructuredBuffer;
+[[vk::binding(3)]] RWStructuredBuffer<uint2> g_primitiveStructuredBufferOutput;
 
 struct S
 {
     float4x4 mat;
 };
 
-[[vk::binding(6)]] StructuredBuffer<S> g_complexStructuredBuffer;
-[[vk::binding(7)]] RWStructuredBuffer<S> g_complexStructuredBufferOutput;
-[[vk::binding(8)]] ConstantBuffer<S> g_cb;
+[[vk::binding(4)]] StructuredBuffer<S> g_complexStructuredBuffer;
+[[vk::binding(5)]] RWStructuredBuffer<S> g_complexStructuredBufferOutput;
+[[vk::binding(6)]] ConstantBuffer<S> g_cb;
 
-[[vk::binding(9)]] cbuffer g_cb
+[[vk::binding(7)]] cbuffer g_cb
 {
     S s;
 }
